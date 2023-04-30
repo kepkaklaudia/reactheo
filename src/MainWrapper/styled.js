ddd7d998109ccbf9623a459451ec1207f20f0739
kepkaklaudia <kepkaklaudia@gmail.com>
2023-04-30 15:30:25 +0200

    MainWrapper added

---
+++ src/MainWrapper/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 0 auto 50px auto;
+  width: 100%;
+  align-content: center;
+  justify-content: center;
+`;
