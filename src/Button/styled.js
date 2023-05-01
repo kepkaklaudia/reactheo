99c06a0d13d63096ba7c689ce53a484ab3d7f6ff
kepkaklaudia <kepkaklaudia@gmail.com>
2023-05-01 17:54:10 +0200

    StyledButton added

---
+++ src/Button/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledButton = styled.button`
+  font-size: 18px;
+  background-color: #087ea4;
+  color: #fff;
+  transition: opacity 0.2s ease-out;
+  width: 120px;
+  height: 50px;
+  border: none;
+  font-weight: 500;
+  border-radius: 30px;
+`;
