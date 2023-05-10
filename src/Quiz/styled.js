4543b58599f0bc5a6234832b61365de8232c3ff6
kepkaklaudia <kepkaklaudia@gmail.com>
2023-05-10 17:40:55 +0200

    Wrapper styles  added

--- src/Quiz/styled.js
+++ src/Quiz/styled.js
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   padding: 20px;
   background-color: #f6f7f9;
+  border-radius: 10px;
+  box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.08);
 `;
