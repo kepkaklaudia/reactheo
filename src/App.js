5306dc15d4755518a29621edffaa9170881b52c2
kepkaklaudia <kepkaklaudia@gmail.com>
2023-07-07 12:12:00 +0200

    renderQuiz added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import { Background } from "./Background";
 import { Header } from "./styled";
 
 function App() {
+  const renderQuiz = () => {
+    return <div>Training mode Exam mode</div>;
+  };
+
   return (
     <>
       <Background />
       <Header>Reactheo</Header>
-      <MainWrapper></MainWrapper>
+      <MainWrapper>{renderQuiz()}</MainWrapper>
     </>
   );
 }
