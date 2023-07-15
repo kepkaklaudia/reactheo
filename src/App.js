01104068583da3c24e3e6232a4c6ed285e593355
kepkaklaudia <kepkaklaudia@gmail.com>
2023-07-15 12:12:00 +0200

    testQuiz added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { MainWrapper } from "./MainWrapper/styled";
 import { Background } from "./Background";
 import { StyledButton } from "./Button/styled";
+import { TestQuiz } from "./Quiz/TestQuiz";
 import { ButtonWrapper, Header, Icon } from "./styled";
 import { Motion } from "./Motion";
 
@@ -17,26 +18,30 @@ function App() {
   };
 
   const renderQuiz = () => {
-    return (
-      <Motion
-        animatedElement={
-          <ButtonWrapper>
-            <div>
-              <Icon>🏋️‍♂️</Icon>
-              <StyledButton start="true" onClick={handleTrainingModeClick}>
-                Training mode
-              </StyledButton>
-            </div>
-            <div>
-              <Icon>🎓</Icon>
-              <StyledButton start="true" onClick={handleExamModeClick}>
-                Exam mode
-              </StyledButton>
-            </div>
-          </ButtonWrapper>
-        }
-      />
-    );
+    if (mode === "training") {
+      return <TestQuiz setMode={setMode} />;
+    } else {
+      return (
+        <Motion
+          animatedElement={
+            <ButtonWrapper>
+              <div>
+                <Icon>🏋️‍♂️</Icon>
+                <StyledButton start="true" onClick={handleTrainingModeClick}>
+                  Training mode
+                </StyledButton>
+              </div>
+              <div>
+                <Icon>🎓</Icon>
+                <StyledButton start="true" onClick={handleExamModeClick}>
+                  Exam mode
+                </StyledButton>
+              </div>
+            </ButtonWrapper>
+          }
+        />
+      );
+    }
   };
 
   return (
