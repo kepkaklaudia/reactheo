2e3322d16fa8faab8b1154737bedfd3d1b4afba4
kepkaklaudia <kepkaklaudia@gmail.com>
2023-07-18 12:12:00 +0200

    main buttons added

--- src/Quiz/TestQuiz.js
+++ src/Quiz/TestQuiz.js
@@ -1,6 +1,14 @@
-export const TestQuiz = () => {
+import { StyledButton } from "../Button/styled";
 
+export const TestQuiz = () => {
   return (
-  <></>
+    <>
+      <StyledButton>Previous</StyledButton>
+
+      <StyledButton>Show Answers</StyledButton>
+      <StyledButton>Hide Answers</StyledButton>
+      <StyledButton>Clear Selections</StyledButton>
+      <StyledButton>Next</StyledButton>
+    </>
   );
 };
