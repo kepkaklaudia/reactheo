97eca500b8c7f98b5a0b3b6cd278304140f8a243
kepkaklaudia <kepkaklaudia@gmail.com>
2023-07-19 12:12:00 +0200

    buttons position changed

--- src/Quiz/TestQuiz.js
+++ src/Quiz/TestQuiz.js
@@ -1,14 +1,16 @@
+import { Buttons } from "./styled";
 import { StyledButton } from "../Button/styled";
 
 export const TestQuiz = () => {
   return (
     <>
-      <StyledButton>Previous</StyledButton>
-
-      <StyledButton>Show Answers</StyledButton>
-      <StyledButton>Hide Answers</StyledButton>
-      <StyledButton>Clear Selections</StyledButton>
-      <StyledButton>Next</StyledButton>
+      <Buttons>
+        <StyledButton>Previous</StyledButton>
+        <StyledButton>Show Answers</StyledButton>
+        <StyledButton>Hide Answers</StyledButton>
+        <StyledButton>Clear Selections</StyledButton>
+        <StyledButton>Next</StyledButton>
+      </Buttons>
     </>
   );
 };
