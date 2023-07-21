7ac69d5e3b4802154c8dd26d0fc087d556da1417
kepkaklaudia <kepkaklaudia@gmail.com>
2023-07-21 12:12:00 +0200

    answers checked added

--- src/Quiz/TestQuiz.js
+++ src/Quiz/TestQuiz.js
@@ -1,18 +1,19 @@
+import { useState } from "react";
 import { Buttons } from "./styled";
 import { StyledButton } from "../Button/styled";
 
 export const TestQuiz = () => {
+  const [answersChecked, setAnswersChecked] = useState(false);
+
   return (
     <>
       <Buttons>
         <StyledButton orderPrevious="true">Previous</StyledButton>
-
         <StyledButton smaller="true">Show Answers</StyledButton>
         <StyledButton smaller="true">Hide Answers</StyledButton>
         <StyledButton smaller="true" span="true">
           Clear Selections
         </StyledButton>
-
         <StyledButton orderNext="true">Next</StyledButton>
       </Buttons>
     </>
