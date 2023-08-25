f297696866dd0cd368efdc688b96d073bd2b7a22
Your Name <kepkaklaudia@gmail.com>
2023-08-25 12:12:00 +0200

    States implemented

--- src/Quiz/ExamQuiz.js
+++ src/Quiz/ExamQuiz.js
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import { useData } from "./getQuestions";
 import { Wrapper } from "./styled";
 import { Loader } from "../Loader/styled";
+
 export const ExamQuiz = ({ setMode }) => {
   const data = useData();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [active, setActive] = useState([]);
+  const [score, setScore] = useState(0);
   const { questions, status } = data;
 
   return status === "loading" ? <Loader /> : <Wrapper></Wrapper>;
